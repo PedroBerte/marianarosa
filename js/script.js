d4caed00de6a0537ffbd2004806cd6b8a5c4d807
PedroBerte <75958199+PedroBerte@users.noreply.github.com>
2021-03-09 21:49:27 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
+$(window).on('beforeunload', function() { //to scroll to top
+    $(window).scrollTop(0);
+});
+
 window.onload = function () {
+
     document.getElementById('inicio-logo').style.opacity = "100%";
+
     var timerId = setTimeout(function(){
         document.getElementById('inicio-logo').style.opacity = "0%";
-    }, 1600);
-    var timerId = setTimeout(function(){
-        document.getElementById('full-screen').style.display = "none";
-    }, 3200);
+    }, 1000);
     var timerId = setTimeout(function(){
         document.getElementById('conteudo-geral').style.opacity = "100%";
-    }, 3100);
+    }, 2000);
+    var timerId = setTimeout(function(){
+        document.getElementById('full-screen').style.display = "none";
+    }, 2000);
+    var timerId = setTimeout(function(){
+        document.getElementById('inicio-logo').style.display = "none";
+    }, 2000);
+
 }
 
 function rolar_para(elemento) {
